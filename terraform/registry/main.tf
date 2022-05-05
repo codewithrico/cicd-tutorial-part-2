361ae5ba6a6a6427545f8ccc8071ef049619a5fb
Rico Fritzsche <rico@bluvolve.dev>
2022-05-05 18:37:56 +0200

    provider version changed

--- terraform/registry/main.tf
+++ terraform/registry/main.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.68.0"
+      version = "~> 3.68.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.1.0"
+      version = "~> 3.1.0"
     }
   }
 }
